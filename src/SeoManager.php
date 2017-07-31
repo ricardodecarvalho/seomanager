1e09cf0d03988ee88993a689a4993c0fc4972d9b
Ricardo de Carvalho <skapitao@gmail.com>
2017-07-31 17:15:57 -0300

    update in return class SeoManager

--- src/SeoManager.php
+++ src/SeoManager.php
@@ -2,8 +2,6 @@
 
 namespace SeoManager;
 
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Doctrine\ORM\EntityManager,
     Doctrine\ORM\Tools\SchemaTool;
 
@@ -11,25 +9,10 @@ class SeoManager
 {
     protected $entityManager;
 
-    protected $isDevMode;
-
     public function __construct(EntityManager $entityManager)
     {
         $this->entityManager = $entityManager;
-    }
-
-    /**
-     * Execute the middleware
-     *
-     * @param ServerRequestInterface $request
-     * @param ResponseInterface $response
-     * @param callable $next
-     * @return mixed
-     */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
-    {
         $this->checkIfTablesExist();
-        return $next($request, $response);
     }
 
     private function checkIfTablesExist()
@@ -45,18 +28,16 @@ class SeoManager
         }
     }
 
-    public function getMetaTags($container)
+    public function getMetaTags($basePath)
     {
-        $basePath = $container['request']->getUri()->getPath();
-
-        $entityManager = $container['em'];
-        $seoManagerRepository = $entityManager->getRepository('SeoManager\Model\Entity\SeoManager');
+        $seoManagerRepository = $this->entityManager->getRepository('SeoManager\Entity\SeoManager');
         $seoManager = $seoManagerRepository->findOneBy(array('url' => $basePath));
-
         if ($seoManager) {
-            $container['view']->offsetSet('title', $seoManager->getTitle());
-            $container['view']->offsetSet('description', $seoManager->getDescription());
-            $container['view']->offsetSet('keywords', $seoManager->getKeywords());
+            return [
+              'title'=> $seoManager->getTitle(),
+              'description'=> $seoManager->getDescription(),
+              'keywords'=> $seoManager->getKeywords(),
+            ];
         }
     }
 }
